e7dba2e6ae1938ecc80b2b5bcd4ac4d50d1c0fd6
shahobiddin2003 <shahobiddinbekmirzaev2003@gmail.com>
2023-08-18 14:54:02 +0300

    good

---
+++ app.js
@@ -0,0 +1,15 @@
+
+let content = document.getElementById("popUp")
+
+
+
+let disappear = function (){
+ 
+   
+content.style.display = 'none'
+
+}
+
+window.onload = function(){
+    content.style.display = 'flex'
+}
